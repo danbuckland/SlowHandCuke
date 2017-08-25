2736e989cd1f3249baa6734f24878324ca9ce2af
Dan Buckland <dan.buckland@chelsea-apps.com>
2017-08-25 11:17:28 +0100

    Overwrite tables correctly to prevent table showing twice in console output

--- lib/slowhandcuke/formatter.rb
+++ lib/slowhandcuke/formatter.rb
@@ -9,8 +9,16 @@ module Slowhandcuke
       @io.flush
     end
 
-    def before_step_result( *args )
-      @io.printf "\r"
+    def before_step_result(*args)
+      is_table = args[2].is_a?(Cucumber::Ast::Table)
+      table_rows = is_table ? args[2].raw.length : 0
+
+      # If there was a table in the step, go up the required number of lines and
+      # print the table result over the top.
+      if is_table
+        @io.printf "\033[#{table_rows + 1}A"
+      end
+      @io.printf "\r\033[K"
       super
     end
   end
